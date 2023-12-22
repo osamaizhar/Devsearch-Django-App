d8a53cdb06b1b14525c13840037e8d71ef544c18
Osama Izhar <46644867+osamaizhar@users.noreply.github.com>
2023-12-22 17:15:23 +0500

    Add files via upload

---
+++ DEVSEARCH/projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views  # . means same file path , importing views here so we can use those functions with these urls
+# Putting all urls related to projects here to organize code better
+
+urlpatterns = [
+    path("projects/",views.projects,name="projects"),
+    path("project/<str:pk>/",views.project,name="project")  
+]
+ 
