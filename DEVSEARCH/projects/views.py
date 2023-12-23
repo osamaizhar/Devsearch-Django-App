aa384bc0ae9fabfd5e85bbe790a952a084e19a1a
osamaizhar <ahmedosamaizhar21@gmail.com>
2023-12-23 15:47:00 +0500

    23/12/23

--- DEVSEARCH/projects/views.py
+++ DEVSEARCH/projects/views.py
@@ -5,8 +5,12 @@ from django.http import HttpResponse # used to return a http response
 
 def projects(request):
     # return HttpResponse("Here are our projects")
-    return render(request,"projects.html") # rendering view from html template using render
+    msg="Helo, you are on Projects Page!"
+    return render(request,"projects/projects.html",{"message":msg}) # rendering view from html template using render
 def project(request,pk):
     #return HttpResponse("Here are our projects "+pk)
-    return render(request,"single-project.html")
+    return render(request,"projects/single-project.html") # we added projects/ because we have made a 
+    # new file structure so that all templates related to projects app are inside the projects app as
+    # projects/templates/projects
+    # root template folder will only have general templates
     
